3bfb572d19caf7925d76642f699e0dc5d345f5dc
Travis Walentin <travis.walentin@gmail.com>
2017-04-06 17:38:48 -0700

    added mongoDB schemas

---
+++ models/listing.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+
+var ListingSchema = mongoose.Schema([{
+    customer:String,
+    time:String, //is this a string?
+    date:Date,
+    company:String,
+    address:String,
+    phoneNumber:String,
+    bio:String, //description?  not 100% on what to call this
+    img:String,  //linked URL
+    service:String,
+    price:Number,
+    duration:Number, // in minutes?
+    rating:Number
+}], {
+    collection: 'listing'
+});
+
+
+module.exports = mongoose.model('listing', ListingSchema);
---
+++ models/provider.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+
+//sub document to provider.  Populate by Provider({services: [{ name: haircut, price: 25, duration: 30},{name:perm,price:200,duration:300}]) Access by provider.services[0].name => haircut 
+
+var ServiceSchema = mongoose.Schema({
+    name:String,
+    price:Number,
+    duration:Number, //in minutes?
+});
+
+var ProviderSchema = mongoose.Schema([{
+    company:String,
+    industry:String,
+    address:String,
+    phoneNumber:String,
+    bio:String, //description?  not 100% on what to call this
+    img:String,  //linked URL
+    services:[serviceSchema],
+    rating:Number
+}], {
+    collection: 'provider'
+});
+
+
+module.exports = mongoose.model('Provider', ProviderSchema);
